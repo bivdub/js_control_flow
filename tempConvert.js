8962ecacb40d77a715c04f1950ee3c3ba4612f25
bivdub <bivdub@gmail.com>
2014-11-19 09:53:55 -0800

    added var keyword

--- tempConvert.js
+++ tempConvert.js
@@ -1,5 +1,5 @@
-tempFar = 75;
+var tempFar = Math.random().toFixed(2) * 100;
 
-tempCel = (((tempFar - 32) * 5) / 9).toFixed(2);
+var tempCel = (((tempFar - 32) * 5) / 9).toFixed(2);
 
 console.log(tempCel + "\xB0C is equal to " + tempFar + "\xB0F");
